a80ea31e8e8ffaf663d5c2930a56c9ccd7f211a2
torin <yurkth@gmail.com>
2020-02-05 05:37:36 +0900

    Update notify

--- board.py
+++ board.py
@@ -1,6 +1,7 @@
 import os
 import re
 from datetime import datetime
+from time import sleep
 
 import requests
 from dhooks import Embed, Webhook
@@ -12,7 +13,7 @@ def generate_embed(url):
     """
     URLのページを確認してEmbedに整形する
     """
-    res = requests.get(url, auth=auth_info)
+    res = requests.get(url, auth=auth_info, timeout=10.0)
     res.encoding = res.apparent_encoding
 
     # 記事の本文
@@ -55,7 +56,39 @@ def generate_embed(url):
 
 def notify():
     hook = Webhook(os.environ["DISCORD_WEBHOOK_URL"])
-    hook.send(embed=generate_embed(os.environ["TEST_ARTICLE_URL"]))
+    # hook.send("@everyone")  # iPhoneで通知のバッチを付ける用
+
+    # 記事一覧を取得
+    year = now.year if (now := datetime.now()).month >= 4 else now.year - 1  # 年度
+    url = f"{os.environ['BOARD_URL']}{year}/boards/new.html"
+    try:
+        res = requests.get(url, auth=auth_info, timeout=10.0)
+    except Exception as e:
+        hook.send(f"ERROR: {e}")
+        return
+    sleep(1)
+    if (status := res.status_code) != requests.codes.ok:
+        hook.send(f"Status code is {status}")
+        return
+    res.encoding = res.apparent_encoding
+    all_articles = re.findall('<A HREF="(.+?)"', res.text)
+
+    # 更新分の取得
+    with open("latest", "r") as f:
+        latest_article = f.read()
+    updated_articles = all_articles[: all_articles.index(latest_article)]
+    if not updated_articles:
+        return
+    try:
+        for article in reversed(updated_articles):
+            hook.send(embed=generate_embed(requests.compat.urljoin(url, article)))
+            sleep(1)
+    except Exception as e:
+        hook.send(f"ERROR: {e}")
+        return
+
+    with open("latest", "w") as f:
+        f.write(article)
 
 
 if __name__ == "__main__":
